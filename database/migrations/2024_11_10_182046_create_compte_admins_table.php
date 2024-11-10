09fc271b8b13553c7d2f49deb57fa774a3252c72
Franck Darren <kikondanze@gmail.com>
2024-11-10 18:34:49 +0100

    fix bdd v2

--- database/migrations/2024_11_10_182046_create_compte_admins_table.php
+++ database/migrations/2024_11_10_182046_create_compte_admins_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('compte_admins', function (Blueprint $table) {
             $table->id();
             $table->integer('solde');
+
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
+
             $table->timestamps();
         });
     }
