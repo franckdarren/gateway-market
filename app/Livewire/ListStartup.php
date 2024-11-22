b80b31326878623cf048276c8889a7709b2062d3
Franck Darren <kikondanze@gmail.com>
2024-11-22 09:08:03 +0100

    ajout depot compte startup

--- app/Livewire/ListStartup.php
+++ app/Livewire/ListStartup.php
@@ -2,27 +2,29 @@
 
 namespace App\Livewire;
 
+use Livewire\Component;
 use App\Models\BonPesee;
 use Filament\Tables\Table;
 use App\Models\CompteAdmin;
 use App\Models\Transaction;
 use App\Models\CompteStartup;
 use App\Models\CompteInvestisseur;
+use Filament\Tables\Actions\Action;
 use Filament\Tables\Filters\Filter;
 use Illuminate\Contracts\View\View;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Contracts\HasTable;
+use Filament\Forms\Components\TextInput;
 use Filament\Tables\Actions\ExportAction;
 use Filament\Tables\Actions\ImportAction;
 use Illuminate\Database\Eloquent\Builder;
 use App\Filament\Exports\BonPeseeExporter;
 use Filament\Tables\Actions\ExportBulkAction;
+
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Actions\Exports\Enums\ExportFormat;
 use Filament\Tables\Concerns\InteractsWithTable;
-
-use Livewire\Component;
 
 class ListStartup extends Component implements HasForms, HasTable
 {
@@ -67,7 +69,33 @@ class ListStartup extends Component implements HasForms, HasTable
             ])
             ->filters([])
             ->actions([
-                // ...
+                // Faire un dépot
+                Action::make('creerTransaction')
+                    ->label("Faire un dépôt d'argent")
+                    ->modalHeading("Faire un dépôt d'argent")
+                    ->form([
+                        TextInput::make('montant')
+                            ->numeric()
+                            ->required()
+                            ->label('Montant'),
+                    ])
+                    ->action(function (array $data, $record) {
+                        // Création manuelle de la transaction sans utiliser la relation morphique
+                        Transaction::create([
+                            'montant' => $data['montant'],
+                            'type' => 'depot',
+                            'description' => "Dépôt d'argent",
+                            'compte_type' => "Compte Startup", // Valeur fixe
+                            'compte_id' => $record->id,
+                            'statut' => 'En attente de traitement',
+                        ]);
+
+                        // Notification::make()
+                        //     ->title('Dépôt effectué')
+                        //     ->success()
+                        //     ->body("Un dépôt de {$data['montant']} a été enregistré pour le compte #{$record->id}.")
+                        //     ->send();
+                    }),
             ])
             ->bulkActions([]);
     }
