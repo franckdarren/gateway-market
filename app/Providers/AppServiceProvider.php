49cf33bce6f371668523c807786b6f6b7f03f27a
Franck Darren <kikondanze@gmail.com>
2024-11-25 13:38:57 +0100

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,5 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        Transaction::observe(TransactionObserver::class);
-    }
+    public function boot(): void {}
 }
