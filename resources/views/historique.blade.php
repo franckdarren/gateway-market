d309f52e9dbda433a93984620bd03e27bf6067a3
isaac omar <isaacomar29@gmail.com>
2024-12-01 16:54:55 +0100

    ajustement inves_offre

--- resources/views/historique.blade.php
+++ resources/views/historique.blade.php
@@ -5,10 +5,8 @@
         </h2>
     </x-slot>
 
-
-    <div class="py-2 bg-[#CFDFEA] lg:py-5">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-
+    <div class="py-2 lg:py-5">
+        <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 @livewire('historique')
             </div>
