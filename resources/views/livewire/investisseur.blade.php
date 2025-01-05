b3277ae29ce142c692c4064b19f9354eeffafc5a
mando_christ <mandochrist13@gmail.com>
2025-01-05 08:45:48 +0100

    contenu du portefeille

--- resources/views/livewire/investisseur.blade.php
+++ resources/views/livewire/investisseur.blade.php
@@ -83,7 +83,7 @@
             </div>
 
             @if (auth()->user()->hasRole('Investisseur'))
-                <div class="w-full xl:max-w-[400px]">
+                <div class="w-full mt-5 md:mt-0 xl:max-w-[400px]">
                     <div class=" flex flex-col mb-10   bg-[#F5F5F5] rounded-2xl container mx-auto">
                         <header class="">
                             <div class="flex border-b-4 border-[#8BC48A] py-5 items-center space-x-5 mx-5">
